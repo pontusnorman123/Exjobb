385bc6aac7a5845a0ec6854616d59f7e9fe9e547
pontusnorman123 <pontusnorman1@gmail.com>
2022-03-24 21:02:59 +0100

    test

--- Environment.py
+++ Environment.py
@@ -5,6 +5,7 @@ from gym import Env
 from gym.spaces import Discrete,Box
 import random
 from enum import Enum
+from pandas import *
 
 class Actions(Enum):
     UPP = 1
@@ -12,61 +13,106 @@ class Actions(Enum):
     DOWN = 3
     LEFT = 4
 
-class Order:
-
-
-    def __init__(self, order_size):
-        self.order = np.chararray(order_size)
-        self.order_types = "abcdefgh"
-        for i in self.order:
-            self.order[i] = np.random.choice(self.order_types)
 
 
 class Warehouse(Env):
 
-    def __init__(self, width, height, order_size):
+    def __init__(self, layout):
 
-        self.width = width
-        self.height = height
+
+        self.layout = layout
+        self.rows = len(self.layout) - 2
+        self.columns = len(self.layout) - 2
 
         # Antal actions som kan tas
         self.action_space = Discrete(4)
+        #self.observation_space = Box()
 
         #start state
-        self.long_pos = 0
-        self.lat_pos = 0
+        self.col_pos = 1
+        self.row_pos = 1
 
-        #delivery size
+        self.order = []
+        #Olika typer av varor där en karaktär representerar en vara
+        self.wares = "abcdefgh"
 
 
+    def set_order(self, order_size):
 
+        for i in range(order_size):
+            ware = random.choice(self.wares)
+            self.order.append(ware)
 
     def step(self, action):
 
-        if action == Actions.UPP:
-            if self.lat_pos == 0:
+        if action == Actions.UPP.value:
+            if self.row_pos == 1:
                 pass
             else:
-                self.lat_pos -= 1
+                self.row_pos -= 1
 
-        if action == Actions.RIGHT:
-            if self.long_pos == self.width:
+        if action == Actions.RIGHT.value:
+            if self.col_pos == self.columns:
                 pass
             else:
-                self.long_pos += 1
+                self.col_pos += 1
 
-        if action == Actions.DOWN:
-            if self.lat_pos == self.height:
+        if action == Actions.DOWN.value:
+            if self.row_pos == self.rows:
                 pass
             else:
-                self.lat_pos += 1
+                self.row_pos += 1
 
-        if action == Actions.LEFT:
-            if self.long_pos == 0:
+        if action == Actions.LEFT.value:
+            if self.col_pos == 0:
                 pass
             else:
-                self.long_pos -= 1
+                self.col_pos -= 1
 
 
         ######### REWARD ###########
+
+        current_pos = self.layout[self.row_pos][self.col_pos]
+        if current_pos in self.order:
+            reward = 1
+        else:
+            reward = 0
+
+        ######## CHECK IF DONE ########
+
+        if not self.order:
+            finished_status = True
+        else:
+            finished_status = False
+
+        info = {}
+
+        return self.row_pos, self.col_pos, reward, finished_status, info
+
+    def reset(self):
+
+        self.row_pos = 1
+        self.col_pos = 1
+
+        return self.row_pos, self.col_pos
+
+    def render(self):
+
+        #Agent representeras med '*'
+
+        current_row = self.layout[self.row_pos]
+        new_row = current_row[:self.col_pos] + '*' + current_row[self.col_pos + 1:]
+        self.layout[self.row_pos] = new_row
+
+        self.print()
+
+        self.layout[self.row_pos] = current_row
+
+
+    def print(self):
+
+        for i in range(self.rows + 2):
+
+            print(self.layout[i])
+
 
--- main.py
+++ main.py
@@ -1,8 +1,56 @@
 import numpy as np
 import Environment
+import random
+
+ENV1 = [
+    ["+-------------------+"],
+    ["|S                  |"],
+    ["| A : : : : : : : : |"],
+    ["| : : : : : : : : : |"],
+    ["| B : : : : : : : : |"],
+    ["| : : : : : : : : : |"],
+    ["| C : : : : : : : : |"],
+    ["+-------------------+"],
+]
+
+ENV2 = [
+    "+-------------------+",
+    "|S                  |",
+    "|                   |",
+    "|  A                |",
+    "|                   |",
+    "|  B                |",
+    "|  C                |",
+    "+-------------------+"
+]
 
 
 if __name__ == '__main__':
 
-    order = Environment.Order(5)
-    print(order.order)
+
+
+    #print(ENV2[1])
+    #print(len(ENV2[0]))
+
+
+    env = Environment.Warehouse(ENV2)
+    env.set_order(5)
+
+    for i in ENV2:
+        print(len(ENV2[i][0]))
+
+
+    # episodes = 10
+    # for episodes in range(1, episodes + 1):
+    #     col_pos, row_pos = env.reset()
+    #     done = False
+    #     score = 0
+    #
+    #     while not done:
+    #
+    #         env.render()
+    #         action = env.action_space.sample()
+    #         col_pos, row_pos, reward, done, info = env.step(action)
+    #         score += reward
+    #
+    #     print('Episodes: {} Score: {}'.format(episodes, score))
