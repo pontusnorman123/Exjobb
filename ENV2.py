f9e963e7ff67cb05e98c061b6fcacb8afe4ea3b5
pontusnorman123 <pontusnorman1@gmail.com>
2022-04-14 14:12:14 +0200

    test

--- ENV2.py
+++ ENV2.py
@@ -29,17 +29,16 @@ class Warehouse(Env):
 
         # Antal actions som kan tas
         self.action_space = Discrete(4)
-        self.actionSpace = {0: -self.columns, 1: self.columns, 2: -1, 3: 1}
+        self.obs = [i for i in range(self.rows*self.columns)]
 
-
-        #self.observation_space = [i for i in range(self.rows*self.columns)]
+        self.observation_space = Box(low=0,high=self.rows*self.columns, shape=(1,))
         #self.observation_space = np.array((self.rows,self.columns))
         #self.observation_space = self.columns, self.rows
 
 
         #start state
-        self.col_pos = 1
-        self.row_pos = 1
+        self.col_pos = 0
+        self.row_pos = 0
         self.agent_pos = 0
 
         #Olika typer av varor där en karaktär representerar en vara
@@ -58,40 +57,45 @@ class Warehouse(Env):
             ware = random.choice(self.wares)
             self.order.append(ware)
 
-
-
-    def get_agent_row_and_column(self):
-
-        x = self.agent_pos // self.columns
-        y = self.agent_pos % self.rows
-        return x,y
-
     def get_agent_pos(self):
-        return self.row_pos * self.rows + self.col_pos
-
-    def off_grid_move(self, new_state, old_state):
-        if new_state not in self.observation_space:
-            return True
-        elif old_state % self.columns and new_state % self.columns == self.columns-1:
-            return True
-        elif old_state % self.columns == self.columns-1 and new_state % self.columns == 0:
-            return True
-        else:
-            return False
+        col_pos = self.agent_pos // self.columns
+        row_pos = self.agent_pos % self.rows
+        return row_pos*col_pos
 
     def step(self, action):
 
         self.steps += 1
-        x,y = self.get_agent_row_and_column()
-        action_res = self.agent_pos + self.actionSpace[action]
 
-        if not self.off_grid_move(action_res, self.agent_pos):
-            self.agent_pos = action_res
+        if action == Actions.UPP.value:
+            if self.row_pos == 0:
+                pass
+            else:
+                self.row_pos -= 1
+                self.agent_pos += -self.columns
+        if action == Actions.RIGHT.value:
+            if self.col_pos == self.columns -1 :
+                pass
+            else:
+                self.col_pos += 1
+                self.agent_pos += 1
+
+        if action == Actions.DOWN.value:
+            if self.row_pos == self.rows - 1:
+                pass
+            else:
+                self.row_pos += 1
+                self.agent_pos += self.columns
+
+        if action == Actions.LEFT.value:
+            if self.col_pos == 0:
+                pass
+            else:
+                self.col_pos -= 1
+                self.agent_pos -=1
 
         ######### REWARD ###########
 
-
-        current_pos = self.layout[y][x]
+        current_pos = self.layout[self.row_pos][self.col_pos]
         if current_pos in self.order:
             reward = self.get_reward()
 
@@ -110,37 +114,31 @@ class Warehouse(Env):
 
         info = {}
 
-        #return self.row_pos, self.col_pos, reward, finished_status, info
+        #state = self.get_agent_pos()
         return self.agent_pos, reward, finished_status, info
 
     def reset(self):
 
-        #self.row_pos = 1
-        #self.col_pos = 1
+        self.row_pos = 0
+        self.col_pos = 0
         self.agent_pos = 0
         self.set_order(self.order_size)
         self.steps = 0
 
         return self.agent_pos
-        #return self.get_agent_pos()
 
     def render(self):
 
 
         #Agent representeras med '*'
 
-        x,y = self.get_agent_row_and_column()
-        #current_row = self.layout[self.row_pos]
-        current_row = self.layout[y]
-        #new_row = current_row[:self.col_pos] + '*' + current_row[self.col_pos + 1:]
-        new_row = current_row[:x] + '*' + current_row[x + 1:]
-        #self.layout[self.row_pos] = new_row
-        self.layout[y] = new_row
+        current_row = self.layout[self.row_pos]
+        new_row = current_row[:self.col_pos] + '*' + current_row[self.col_pos + 1:]
+        self.layout[self.row_pos] = new_row
 
         self.print()
 
-        #self.layout[self.row_pos] = current_row
-        self.layout[y] = current_row
+        self.layout[self.row_pos] = current_row
 
 
     def print(self):
--- main.py
+++ main.py
@@ -1,32 +1,23 @@
+import keras
 import numpy as np
+
 import Environment
+import ENV2
 import random
+import sys,os
+
+os.add_dll_directory("C:/Program/NVIDIA GPU Computing Toolkit/CUDA/v10.0/bin")
+
 from keras.models import Sequential
-from keras.layers import Dense, Flatten
-from keras.optimizers import Adam
+from keras.layers import Dense, Flatten,Input
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import datetime
 
 
-ENV1 = [
-    ["+-------------------+"],
-    ["|S                  |"],
-    ["| A : : : : : : : : |"],
-    ["| : : : : : : : : : |"],
-    ["| B : : : : : : : : |"],
-    ["| : : : : : : : : : |"],
-    ["| C : : : : : : : : |"],
-    ["+-------------------+"],
-]
-
-ENV2 = [
-    "+-------------------+",
-    "|S                  |",
-    "|                   |",
-    "|  A                |",
-    "|                   |",
-    "|  B                |",
-    "|  C                |",
-    "+-------------------+"
-]
 
 ENV3 = [
     "+-------------------+",
@@ -38,33 +29,101 @@ ENV3 = [
     "+-------------------+"
 ]
 
+ENV = [
+    "+-------------------+",
+    "|S                  |",
+    "|    A   B    C     |",
+    "|    D   E    F     |",
+    "|    G   H    I     |",
+    "|    J   K    L     |",
+    "+-------------------+"
+]
+
+ENV = [
+
+    "S                  ",
+    "    A   B    C     ",
+    "    D   E    F     ",
+    "    G   H    I     ",
+    "    J   K    L     ",
+]
+
 ORDER_SIZE = 20
 
 if __name__ == '__main__':
 
 
-
-    #print(len(ENV2))
-    #print(len(ENV2[0]))
-
-
-    env = Environment.Warehouse(ENV3)
+    x = np.array([0,0])
+    print(x)
 
 
-    episodes = 1000
+
+    env = ENV2.Warehouse(ENV, ORDER_SIZE)
+
+    # print(env.rows,env.columns)
+    # # print(env.observation_space)
+    # #
+    episodes = 0
     for episodes in range(1, episodes + 1):
-        col_pos, row_pos = env.reset(ORDER_SIZE)
+        agent_pos = env.reset()
+        #test = env.reset(ORDER_SIZE)
         done = False
         score = 0
-        #print(env.order)
+        print(env.order)
 
 
         while not done:
 
-            #env.render()
+            print("Agent_pos:",env.agent_pos)
+            env.render()
             action = env.action_space.sample()
-            #print(action)
-            col_pos, row_pos, reward, done, info = env.step(action)
+            print("Action:", action)
+            agent_pos, reward, done, info = env.step(action)
+
             score += reward
 
         print('Episodes: {} Score: {}'.format(episodes, score))
+
+
+
+
+    actions = env.action_space.n
+    print("Number of Actions:", actions) #S: Denna borde vara 4. för det är 4 actions.
+
+    inputs = env.observation_space.shape
+    print("Inputs:", inputs) #S: Osäker kring denna... Den borde vara (95) eller (5, 19) eller (7, 21) eller nått annat beroende på hur din observerbara 'karta' ser ut...
+
+
+
+    model = Sequential()
+    model.add(Dense(units=24, input_shape=inputs, activation='relu'))
+    #model.add(Flatten())
+    model.add(Dense(units=24, activation='relu'))
+    model.add(Dense(units=24, activation='relu'))
+    #model.add(Flatten())
+    model.add(Dense(units=actions, activation='linear'))
+    model.summary()
+    print(model.output_shape)
+
+    ######
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+
+    opt = tf.keras.optimizers.Adam(learning_rate = 0.1)
+    dqn.compile(opt, metrics=['mae'])
+
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    history = dqn.fit(env, nb_steps=5000, visualize=False, verbose=2)
+
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+    history.
