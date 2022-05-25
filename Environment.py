209668312819eff3049cc5ec2a322101a8986718
pontusnorman123 <pontusnorman1@gmail.com>
2022-05-26 00:09:48 +0200

    test

--- Environment.py
+++ Environment.py
@@ -1,12 +1,10 @@
-import string
 
-import numpy
 import numpy as np
 from gym import Env
 from gym.spaces import Discrete,Box
 import random
 from enum import Enum
-from pandas import *
+
 
 class Actions(Enum):
     UPP = 0
@@ -18,86 +16,76 @@ class Actions(Enum):
 
 class Warehouse(Env):
 
-    def __init__(self, layout, order_size):
+    def __init__(self, layout):
 
 
         self.layout = layout
         self.rows = len(self.layout)
         self.columns = len(self.layout[0])
-        self.grid = np.zeros((self.rows,self.columns))
         self.size = self.rows*self.columns
 
-        # Antal actions som kan tas
+
         self.action_space = Discrete(4)
-        self.actionSpace = {0: -self.columns, 1: self.columns, 2: -1, 3: 1}
 
-
-        #self.observation_space = [i for i in range(self.rows*self.columns)]
-        #self.observation_space = np.array((self.rows,self.columns))
-        #self.observation_space = self.columns, self.rows
-
+        self.observation_space = Box(low=np.array([0]),high=np.array([self.rows*self.columns]),dtype=int)
 
         #start state
-        self.col_pos = 1
-        self.row_pos = 1
+        self.col_pos = 0
+        self.row_pos = 0
         self.agent_pos = 0
 
-        #Olika typer av varor där en karaktär representerar en vara
-        self.wares = "ABCDEFGHIJKL"
 
-        #Varorna läggs i denna array
-        #self.order = ['A','B','C','D','E','F','G','H']
-        self.order_size = order_size
+        #Objects that should be picked up, they should match the objects in the chosen environment
 
-        #Antal steg som agenten tagit
-        self.steps = 0
-
-    def set_order(self, order_size):
-
-        for i in range(order_size):
-            ware = random.choice(self.wares)
-            self.order.append(ware)
+        #self.order = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N']
+        #self.order = ['A','B','C']
+        #self.order = ['A', 'B', 'C', 'D', 'E']
+        self.order = ['A']
 
 
-
-    def get_agent_row_and_column(self):
-
-        x = self.agent_pos // self.columns
-        y = self.agent_pos % self.rows
-        return x,y
-
-    def get_agent_pos(self):
-        return self.row_pos * self.rows + self.col_pos
-
-    def off_grid_move(self, new_state, old_state):
-        if new_state not in self.observation_space:
-            return True
-        elif old_state % self.columns and new_state % self.columns == self.columns-1:
-            return True
-        elif old_state % self.columns == self.columns-1 and new_state % self.columns == 0:
-            return True
-        else:
-            return False
 
     def step(self, action):
 
-        self.steps += 1
-        x,y = self.get_agent_row_and_column()
-        action_res = self.agent_pos + self.actionSpace[action]
 
-        if not self.off_grid_move(action_res, self.agent_pos):
-            self.agent_pos = action_res
+        self.steps += 1
+
+        if action == Actions.UPP.value:
+            if self.row_pos == 0:
+                pass
+
+            else:
+                self.row_pos = self.row_pos - 1
+                self.agent_pos = self.agent_pos - self.columns
+        if action == Actions.RIGHT.value:
+            if self.col_pos == self.columns -1 :
+                pass
+            else:
+                self.col_pos = self.col_pos + 1
+                self.agent_pos = self.agent_pos + 1
+
+        if action == Actions.DOWN.value:
+            if self.row_pos == self.rows - 1:
+                pass
+            else:
+                self.row_pos = self.row_pos + 1
+                self.agent_pos = self.agent_pos + self.columns
+
+        if action == Actions.LEFT.value:
+            if self.col_pos == 0:
+                pass
+            else:
+                self.col_pos = self.col_pos - 1
+                self.agent_pos = self.agent_pos - 1
+
+        current_pos = self.layout[self.row_pos][self.col_pos]
 
         ######### REWARD ###########
 
 
-        current_pos = self.layout[y][x]
+
         if current_pos in self.order:
-            reward = self.get_reward()
-
-
+            reward = 1
             self.order.remove(current_pos)
-
         else:
             reward = 0
 
@@ -110,37 +98,34 @@ class Warehouse(Env):
 
         info = {}
 
-        #return self.row_pos, self.col_pos, reward, finished_status, info
         return self.agent_pos, reward, finished_status, info
 
     def reset(self):
 
-        #self.row_pos = 1
-        #self.col_pos = 1
+
+        self.row_pos = 0
+        self.col_pos = 0
         self.agent_pos = 0
-        self.set_order()
+        #self.order = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N']
+        #self.order = ['A', 'B', 'C', 'D', 'E']
+        #self.order = ['A','B','C']
+        self.order = ['A']
         self.steps = 0
 
         return self.agent_pos
-        #return self.get_agent_pos()
 
     def render(self):
 
 
-        #Agent representeras med '*'
+        #Agent represented with '*'
 
-        x,y = self.get_agent_row_and_column()
-        #current_row = self.layout[self.row_pos]
-        current_row = self.layout[y]
-        #new_row = current_row[:self.col_pos] + '*' + current_row[self.col_pos + 1:]
-        new_row = current_row[:x] + '*' + current_row[x + 1:]
-        #self.layout[self.row_pos] = new_row
-        self.layout[y] = new_row
+        current_row = self.layout[self.row_pos]
+        new_row = current_row[:self.col_pos] + '*' + current_row[self.col_pos + 1:]
+        self.layout[self.row_pos] = new_row
 
         self.print()
 
-        #self.layout[self.row_pos] = current_row
-        self.layout[y] = current_row
+        self.layout[self.row_pos] = current_row
 
 
     def print(self):
@@ -155,7 +140,3 @@ class Warehouse(Env):
 
         print("+-------------+")
 
-
-    def get_reward(self):
-
-        return 1/self.steps
--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import random
 
 import numpy as np
-import ENV2
+import Environment
 import ShowerEnv
 import os
 os.add_dll_directory("C:/Program/NVIDIA GPU Computing Toolkit/CUDA/v10.0/bin")
 from keras.models import Sequential
-from keras.layers import Dense, Flatten,Input
+from keras.layers import Dense, Flatten,Input, Embedding,Reshape
 from rl.agents import DQNAgent
 from rl.policy import BoltzmannQPolicy
+from rl.policy import EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 import tensorflow as tf
 from matplotlib import pyplot as plt
 import gym
+import keras.optimizer_v2.learning_rate_schedule
+import keras.callbacks
+import keras.losses
+import collections
+import keras.callbacks_v1
+import time
+import datetime
+
+
 
 ENV = [
 
@@ -21,144 +31,219 @@ ENV = [
 
 ]
 
+ENV2 = [
+
+    "S A",
+    "   ",
+    "B C"
+
+]
+
 ENV3 = [
 
-    "S   A",
-    "B   C"
+    "S A B",
+    "C D E",
+    "F G H",
+    "I J K",
+    "L M N"
 
 ]
 
 ENV4 = [
 
-    "S   A    B",
-    "C   D    E",
-    "F   G    H"
+    "S   A",
+    "     ",
+    "B   C",
+    "     ",
+    "D   E"
 
 ]
 
-ORDER_SIZE = 20
 
 if __name__ == '__main__':
 
 
 
-    env2 = gym.make("FrozenLake-v1")
-    print(env2.action_space)
-    print(env2.observation_space)
-
-    env = ENV2.Warehouse(ENV4, ORDER_SIZE)
-    envo = ShowerEnv.ShowerEnv()
+    #chose environment ENV,ENV2,ENV3 or ENV4
+    env = Environment.Warehouse(ENV)
 
 
-    ######################################################
 
-
+    #Action space and observation space to initialize Q-table
     action_space_size = env.action_space.n
-    state_space_size = env.size
-    q_table = np.zeros((state_space_size,action_space_size))
+    observation_space_size = env.size
+    q_table = np.zeros((observation_space_size,action_space_size))
 
-    num_episodes = 80000
-    max_steps_per_episodes = 300
+    # Number of episodes that the reinforcement learning model should train for
+    num_episodes = 10000
+    # Max number of training steps per episode
+    max_steps_per_episodes = 100
+    # Max number of steps for testing model
+    max_steps_testing = 6
+
+    # Q-learnig algorithm paramaters
     learning_rate = 0.1
-    discount_rate = 0.99
+    discount_rate = 0.0000001
     exploration_rate = 1
     max_exploration_rate = 1
     min_exploration_rate = 0.1
-    exploration_decay_rate = 0.01
+    exploration_decay_rate = 0.001
 
-    reward_all_episodes = []
+    # Lists to save data
+    reward_all_episodes_training = []
+    steps = []
+    times = []
+    reward_all_episodes_testing = []
 
-    for episode in range(num_episodes):
+    #Training_episodes is how many times the model is retrained
+    training_episodes = 0
+    for i in range(training_episodes):
 
-        state = env.reset()
-        done = False
-        rewards_current_episode = 0
+        ###### Manually implemented Q-learning algorithm ###### START
+        start = time.time()
+        for episode in range(num_episodes):
 
-        for step in range(max_steps_per_episodes):
-        #while not done:
-            exploration_rate_threshold = random.uniform(0,1)
-            if exploration_rate_threshold > exploration_rate:
-                action = np.argmax(q_table[state,:])
-            else:
-                action = env.action_space.sample()
+            state = env.reset()
+            done = False
+            rewards_current_episode_training = 0
 
-            new_state, reward, done, info = env.step(action)
+            for step in range(max_steps_per_episodes):
+                exploration_rate_threshold = random.uniform(0,1)
+                if exploration_rate_threshold > exploration_rate:
+                    action = np.argmax(q_table[state,:])
+                else:
+                    action = env.action_space.sample()
+                new_state, reward, done, info = env.step(action)
 
-            q_table[state,action] = q_table[state,action] * (1 - learning_rate) + \
-                learning_rate * (reward + discount_rate * np.max(q_table[new_state, :]))
 
-            state = new_state
-            rewards_current_episode += reward
+                q_table[state,action] = q_table[state,action] * (1 - learning_rate) + \
+                    learning_rate * (reward + discount_rate * np.max(q_table[new_state, :]))
 
-            if done:
-                break
+                state = new_state
+                rewards_current_episode_training += reward
+                if done:
+                    break
 
-        exploration_rate = min_exploration_rate + (max_exploration_rate - min_exploration_rate)\
-            * np.exp(-exploration_decay_rate * episode)
+            exploration_rate = min_exploration_rate + (max_exploration_rate - min_exploration_rate)\
+                * np.exp(-exploration_decay_rate * episode)
 
-        reward_all_episodes.append(rewards_current_episode)
+            reward_all_episodes_training.append(rewards_current_episode_training)
 
-    rewards_per_thousand_episodes = np.split(np.array(reward_all_episodes), num_episodes/1000)
-    count = 1000
-    print("***********Average reward per thousand episodes********\n")
+        ###### Manually implemented Q-learning algorithm ###### END
+
+        end = time.time()
+        dif= end-start
+        times.append(dif)
+
+        ########## Testing agent ########
+
+        for episode in range(num_episodes):
+
+
+            state = env.reset()
+            done = False
+            rewards_current_episode_testing = 0
+
+
+            for step in range(max_steps_testing):
+
+
+                action = np.argmax(q_table[state, :])
+                new_state, reward, done, info = env.step(action)
+                state = new_state
+                rewards_current_episode_testing += reward
+                if done:
+                    break
+
+
+
+            reward_all_episodes_testing.append(rewards_current_episode_testing)
+
+
+
+    counter = collections.Counter(reward_all_episodes_testing)
+    rewards_per_thousand_episodes = np.array_split(np.array(reward_all_episodes_training), num_episodes/100)
+
+
+    ###### saves the avrage reward per hundred epiosdes and plots ####
+    count = 100
+    rewards_mean=[]
+    print(counter)
     for r in rewards_per_thousand_episodes:
-        print(count, ": ", str(sum(r/1000)))
-        count += 1000
+         rewards_mean.append(sum(r / len(r)))
+         #print(count, ": ", str(sum(r/100)))
+         #count += 100
 
-    print(q_table)
-    ######################################################
+    plt.xlabel('Episodes in hundreds')
+    plt.ylabel('Reward')
+    plt.plot(rewards_mean)
+    plt.savefig("graph.png")
+    plt.show()
 
+    ############## TensorFlow implementation ############
 
+    # Action space and observation space
     actions = env.action_space.n
-    print("Number of Actions:", actions) #S: Denna borde vara 4. för det är 4 actions.
-
     inputs = env.observation_space.shape
-    print(env.observation_space)
-    print("Inputs:", inputs) #S: Osäker kring denna... Den borde vara (95) eller (5, 19) eller (7, 21) eller nått annat beroende på hur din observerbara 'karta' ser ut...
 
 
+    # Neural network model
+    model = Sequential()
+    model.add(Dense(units = 16,input_shape=inputs, activation='relu'))
+    model.add(Dense(units=32, activation='relu'))
+    model.add(Dense(units=32, activation='relu'))
+    model.add(Dense(units=actions, activation='linear'))
+    model.summary()
+    print(model.output_shape)
 
-    # model = Sequential()
-    # model.add(Dense(units=32, input_shape=inputs, activation='relu'))
-    # model.add(Dense(units=32, activation='relu'))
-    # model.add(Dense(units=32, activation='relu'))
-    # model.add(Dense(units=actions, activation='linear'))
-    # model.summary()
-    # print(model.output_shape)
-    #
-    # ######
-    # policy = BoltzmannQPolicy()
-    # memory = SequentialMemory(limit=500000, window_length=1)
-    # dqn = DQNAgent(model=model,gamma=0.99, memory=memory ,policy=policy, nb_actions=actions, nb_steps_warmup=100, target_model_update=0.2)
-    #
-    # opt = tf.keras.optimizers.Adam(learning_rate = 0.1)
-    # dqn.compile(opt, metrics=['mae'])
-    #
-    #
-    # #dqn.load_weights("test")
-    # history = dqn.fit(env, nb_max_episode_steps=50, nb_steps=500000, visualize=False, verbose=2 )
-    #
-    # print(history.history.keys())
-    #
-    # train_rewards = history.history['episode_reward']
-    # #dqn.save_weights("test")
-    #
-    #
-    # rewards = np.array_split(train_rewards, len(train_rewards)/100)
-    # #print(rewards)
-    # count = 100
-    # x=[]
-    # print("***********Average reward per thousand episodes********\n")
-    # for r in rewards:
-    #     x.append(sum(r/len(r)))
-    #     print(count, ": ", str(sum(r/len(r))))
-    #     count += 100
-    #
-    # #plt.xlim([0,1000])
-    # plt.plot(x)
-    # plt.show()
-    #
-    #
-    #
-    # #x = dqn.test(env, nb_episodes=5, visualize=False)
-    #
+
+    ### Neural network paramaters
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=500000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory ,policy=policy, nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+    opt = tf.keras.optimizers.Adam(learning_rate = 0.1)
+    dqn.compile(opt, metrics=['accuracy'])
+
+    #Training_episodes is how many times the model is retrained
+    training_episodes = 1
+    #Number of steps for each episode
+    nb_steps = 10000
+
+    test_rewards = []
+    nb_episodes = []
+    times = []
+
+    for i in range(training_episodes):
+
+        #Training
+        start = time.time()
+        history = dqn.fit(env,nb_steps=nb_steps, visualize=False, verbose=1)
+        end = time.time()
+        dif=end - start
+        times.append(dif)
+
+
+        train_rewards = history.history['episode_reward']
+        nb_episodes.append(len(train_rewards))
+
+        rewards = np.array_split(train_rewards, len(train_rewards)/100)
+
+        rewards_mean = []
+        for r in rewards:
+            rewards_mean.append(sum(r/len(r)))
+
+        plt.plot(rewards_mean)
+        plt.xlabel('Episodes in thousands')
+        plt.ylabel('Reward')
+
+        plt.show()
+
+        ## Test model
+        test_history = dqn.test(env, nb_episodes=1, visualize=False,nb_max_episode_steps=2,verbose=1)
+
+
+    counter = collections.Counter(history.history['episode_reward'])
+    print(counter)
+    print("nb_steps",nb_steps)
+    print("nb_episodes_mean",np.mean(nb_episodes))
+    print("time: ", np.mean(times))
